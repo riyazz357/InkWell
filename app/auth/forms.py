86dee30337d52c3ac23a62822c24f4cd5f917a6f
mohammad riyazuddin <mohammadriyazuddin494@gmail.com>
2025-10-05 23:40:43 +0530

    change the route setting

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -16,12 +16,12 @@ class RegistrationForm(FlaskForm):
     password2=PasswordField('Repeat Password',validators=[DataRequired(),EqualTo('password')])
     submit=SubmitField('Register')
 
-    def validate_username(self,username):
-        user=User.query.filter_by(username=username.data).first()
+    def validate_username(self,username): #custom validator
+        user=User.query.filter_by(username=username.data).first() #tablename.query.filter_by(similar to where condition)
         if user is not None:
             raise ValidationError("plesae use a different username")
 
     def validate_email(self,email):
-        user=User.query.filter_by(email=email.data).first()
+        user=User.query.filter_by(email=email.data).first() #query to stop as soon as it finds one matching result and return it
         if user is not None:
             raise ValidationError("please use a different email address")
